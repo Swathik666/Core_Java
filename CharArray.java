9d4ff3e00491a4edadcf5dc0d5f811e61fc46dcd
Swathik666 <114558659+Swathik666@users.noreply.github.com>
2022-09-28 11:18:10 +0530

    Add files via upload

---
+++ CharArray.java
@@ -0,0 +1,12 @@
+class Marksheet{
+          public static void main(String []args){
+             char name1='s';
+             char name2='w';
+             char name3='a';
+             char name4='t';
+             char name5='h';
+             char name6='i';
+  System.out.println(name1+""+name2+""+ name3+""+name4+""+name5+""+name6);
+     }
+
+}
---
+++ condstmt.java
@@ -0,0 +1,13 @@
+class Blackmail{
+	public static void main(String[]arg){
+
+	short expected=4000;
+	short real=4600;
+ 	if(expected<real){
+	System.out.println("not reveal secret");
+	}
+	else{
+	System.out.println("reveal the secret");
+		}
+	}
+}
